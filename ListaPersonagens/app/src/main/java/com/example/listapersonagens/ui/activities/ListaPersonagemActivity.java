128bbc3ab16a9769070157fcbf0667f4cd304b8b
Henrique <henriqueanselmo1011@gmail.com>
2021-04-16 20:34:39 -0300

    Feito em aula 16.04

--- ListaPersonagens/app/src/main/java/com/example/listapersonagens/ui/activities/ListaPersonagemActivity.java
+++ ListaPersonagens/app/src/main/java/com/example/listapersonagens/ui/activities/ListaPersonagemActivity.java
@@ -1,5 +1,7 @@
 package com.example.listapersonagens.ui.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -78,16 +80,36 @@ public class ListaPersonagemActivity extends AppCompatActivity {
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
-        menu.add("Remover");
+        //menu.add("Remover");
+        //menu.add("Teste");
+        getMenuInflater().inflate(R.menu.activity_lista_personagens_menu, menu);
     }
 
     @Override
     //Pega a informação e deleta
-    public boolean onContextItemSelected(@NonNull MenuItem item) {
-        AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-        Personagem personagemEscolhido = adapter.getItem(menuInfo.position);
-        remove(personagemEscolhido);
+    public boolean onContextItemSelected( MenuItem item) {
+        configuraMenu(item);
         return super.onContextItemSelected(item);
+    }
+
+    private void configuraMenu(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.activity_lista_pesonagem_menu_remover) {
+
+            new AlertDialog.Builder(this).setTitle("Removendo Personagem").setMessage("Tem certeza que deseja remover?").setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+                    Personagem personagemEscolhido = adapter.getItem(menuInfo.position);
+                    remove(personagemEscolhido);
+                }
+            })
+                    .setNegativeButton("Não", null).show();
+
+
+
+
+        }
     }
 
     //faz a configuração da lista
